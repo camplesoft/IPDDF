0e721e437875f03e9e5b17937436d5843439f765
camplesoft <camplesoft@gmail.com>
2024-03-03 22:07:15 +0000

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+import { Header, Footer } from "./elementos_reaproveitaveis.js";
+document.head.innerHTML +=
+  '<link rel="stylesheet" href="../styles/style_header_and_footer.css">';
+
+const header = new Header();
+const footer = new Footer();
+header.showElements();
+footer.showElements();
+
