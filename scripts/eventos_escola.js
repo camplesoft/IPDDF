08d1afcdf284bbf76b8c8ffbc629f2557f3d33f1
Mário <mariomakondambuta@gmail.com>
2024-03-12 11:56:50 +0000

    correção

--- scripts/eventos_escola.js
+++ scripts/eventos_escola.js
@@ -2,70 +2,70 @@ export default [
     {
       title: "campanha de arborização",
       img: {
-        src: "../images/eventos-escola/campanha_de_arborizacao.jpg",
+        src: "images/eventos-escola/campanha_de_arborizacao.jpg",
         alt: "campanha_de_arborizacao"
       }
     },
     {
       title: "dia do artista",
       img: {
-        src: "../images/eventos-escola/dia_do_artista.jpg",
+        src: "images/eventos-escola/dia_do_artista.jpg",
         alt: "dia_do_artista"
       }
     },
     {
       title: "dia mundial da luta contra o suicídio",
       img: {
-        src: "../images/eventos-escola/dia_mundial_da_luta_contra_o_suicidio.jpg",
+        src: "images/eventos-escola/dia_mundial_da_luta_contra_o_suicidio.jpg",
         alt: "dia_mundial_da_luta_contra_o_suicidio"
       }
     },
     {
       title: "dia mundial da saúde mental",
       img: {
-        src: "../images/eventos-escola/dia_mundial_da_saude_mental.jpg",
+        src: "images/eventos-escola/dia_mundial_da_saude_mental.jpg",
         alt: "dia_mundial_da_saude_mental"
       }
     },
     {
       title: "dia mundial do professor",
       img: {
-        src: "../images/eventos-escola/dia_mundial_do_professor.jpg",
+        src: "images/eventos-escola/dia_mundial_do_professor.jpg",
         alt: "dia_mundial_do_professor"
       }
     },
     {
       title: "dia mundial do sonho",
       img: {
-        src: "../images/eventos-escola/dia_mundial_do_sonho.jpg",
+        src: "images/eventos-escola/dia_mundial_do_sonho.jpg",
         alt: "dia_mundial_do_sonho"
       }
     },
     {
       title: "mês da biblia",
       img: {
-        src: "../images/eventos-escola/mes_da_biblia.jpg",
+        src: "images/eventos-escola/mes_da_biblia.jpg",
         alt: "mes_da_biblia"
       }
     },
     {
       title: "outubro rosa",
       img: {
-        src: "../images/eventos-escola/outubro_rosa.jpg",
+        src: "images/eventos-escola/outubro_rosa.jpg",
         alt: "outubro_rosa"
       }
     },
     {
       title: "palestra de prevenção ao suicídio",
       img: {
-        src: "../images/eventos-escola/palestra_de_prevencao_ao_suicidio.jpg",
+        src: "images/eventos-escola/palestra_de_prevencao_ao_suicidio.jpg",
         alt: "palestra_de_prevencao_ao_suicidio"
       }
     },
     {
       title: "santo do mês",
       img: {
-        src: "../images/eventos-escola/santo_do_mes.jpg",
+        src: "images/eventos-escola/santo_do_mes.jpg",
         alt: "santo_do_mes"
       }
     }
