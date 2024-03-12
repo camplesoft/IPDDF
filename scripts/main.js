4492f44ce99e7e4a125e01e49f6160db467a8743
Mário <mariomakondambuta@gmail.com>
2024-03-12 09:21:37 +0000

    link de estilização do header e footer

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 import { Header, Footer } from "./elementos_reaproveitaveis.js";
 document.head.innerHTML +=
-  '<link rel="stylesheet" href="../styles/style_header_and_footer.css">';
+  '<link rel="stylesheet" href="styles/style_header_and_footer.css">';
 
 const header = new Header();
 const footer = new Footer();
